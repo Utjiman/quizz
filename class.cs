971e66613e9e8c3a1d6f7de03fb19cc9b7a30c55
Dan Lund <dan.lundh@outlook.com>
2023-10-12 11:18:46 +0200

    Strul 1 	modified:   class.cs

--- class.cs
+++ class.cs
@@ -7,6 +7,37 @@ public Quiz(string thequerytext)
     Thequerytext = thequerytext;
 }
 
-
 }
 
+public class Stringanswer : Quiz
+{
+    public string correct {set; get;}
+    public string thequerytext;
+
+    public Stringanswer(string correct, string thequerytext) : base(thequerytext)
+    {
+        this.correct =correct;
+}
+
+}
+class YearQuestion : Quiz
+{
+    public int year {get; set;}
+}
+
+class MultipleChoises : Questions
+{
+    List<MultipleChoises> Choices = new List<MultipleChoises>();
+    public string choice;
+    //add funktion
+    //ta bort funktion
+}
+class NumericQuestion : Questions
+{
+    //minValue
+    //MaxValue
+}
+class TrueOrFalseQuestion : Questions
+{
+    public bool IsTrue {get; set;}
+}
