619c8c1529d6c20c7ec555a1a76e176b7763c646
Richard Bodén <utjis@hotmail.com>
2023-10-12 11:07:46 +0200

    Added class and moved them.

--- class.cs
+++ class.cs
@@ -1,4 +1,5 @@
-public class Quiz{
+public class Quiz
+{
 
 public string Thequerytext {get; set;}
 public Quiz(string thequerytext)
@@ -7,39 +8,5 @@ public Quiz(string thequerytext)
 }
 
 
-
-
 }
 
-public class Stringanswer : Quiz
-{
-    public string correct {set; get;}
-    public string thequerytext;
-
-    public Stringanswer(string correct, string thequerytext) : base(thequerytext)
-    {
-        this.correct =correct;
-    }
-
-}
-class YearQuestion : Quiz
-{
-    public int year {get; set;}
-}
-
-class MultipleChoises : Questions
-{
-    List<MultipleChoises> Choices = new List<MultipleChoises>();
-    public string choice;
-    //add funktion
-    //ta bort funktion
-}
-class NumericQuestion : Questions
-{
-    //minValue
-    //MaxValue
-}
-class TrueOrFalseQuestion : Questions
-{
-    public bool IsTrue {get; set;}
-}
