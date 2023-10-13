28275b72ed8218662e1b867fc5dc15a6c2c5540a
Dan Lund <dan.lundh@outlook.com>
2023-10-13 13:11:25 +0200

    Outline  fix

--- class1.cs
+++ class1.cs
@@ -5,7 +5,7 @@ public class Stringanswer : Quiz
 
     public Stringanswer(string correct, string thequerytext) : base(thequerytext)
     {
-        this.correct =correct;
+        this.correct =correct; 
     }
 
 }
@@ -15,10 +15,23 @@ public class Stringanswer : Quiz
 class NumericQuestion : Quiz
 {
     
+    public int number {set; get;}
+    public NumericQuestion(int number, string thequerytext): base(thequerytext)
+    {
+        this.number = number;
+    }
+
+    
+
 }
 
 class ThreeChoises : Quiz
 {
     public string Pick {get; set;}
+    public ThreeChoises(string Pick, string thequerytext): base(thequerytext)
+    {
+        this.Pick = Pick;
+    }
+
 }
 
